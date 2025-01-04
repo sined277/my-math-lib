42c3c073c09ca2ced486366335a389a371a85726
vLomakaWork <vLomakaWork@Vitaliis-MacBook-Pro.local>
2025-01-04 12:03:00 +0200

    fix: issue 4

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,10 @@ export function add(a: number, b: number): number {
 }
 
 export function divide(a: number, b: number): number | string {
+  if (b === 0) {
+    // throw new Error("Divide by zero isn't allowed");
+    return 'Division by zero is not allowed';
+  }
   return MyMathLib.divide(a, b);
 }
 
