c788d4eabd29479ffd3babf48f291db614350300
vLomakaWork <vLomakaWork@Vitaliis-MacBook-Pro.local>
2025-01-04 10:56:14 +0200

    fix: issue 3

--- ios/MyMathLib.mm
+++ ios/MyMathLib.mm
@@ -5,13 +5,29 @@ RCT_EXPORT_MODULE()
 
 - (NSNumber *)multiply:(double)a b:(double)b {
     NSNumber *result = @(a * b);
+    return result;
+}
 
+- (NSNumber *)add:(double)a b:(double)b {
+    NSNumber *result = @(a + b);
+    return result;
+}
+
+- (NSNumber *)subtract:(double)a b:(double)b {
+    NSNumber *result = @(a - b);
+    return result;
+}
+
+- (id)divide:(double)a b:(double)b {
+    if (b == 0) {
+        return @"Error: Division by zero";
+    }
+    NSNumber *result = @(a / b);
     return result;
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
+    (const facebook::react::ObjCTurboModule::InitParams &)params {
     return std::make_shared<facebook::react::NativeMyMathLibSpecJSI>(params);
 }
 
