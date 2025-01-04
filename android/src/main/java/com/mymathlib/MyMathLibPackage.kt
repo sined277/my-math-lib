fd78f9d256e2440aa277d9b26221c1f01fa5b2b3
vLomakaWork <vLomakaWork@Vitaliis-MacBook-Pro.local>
2025-01-05 01:49:15 +0200

    fix: issue 12 android

--- android/src/main/java/com/mymathlib/MyMathLibPackage.kt
+++ android/src/main/java/com/mymathlib/MyMathLibPackage.kt
@@ -3,7 +3,11 @@ package com.mymathlib
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.uimanager.ViewManager
+import com.facebook.react.bridge.ViewManager
+import com.facebook.react.bridge.ReactModuleInfoProvider
+
+import com.facebook.react.bridge.ReactModuleInfoProvider
+
 
 class MathPackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
