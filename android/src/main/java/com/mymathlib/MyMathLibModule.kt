d0615bd5846952b9c6083c80496dc11441f8fc45
vLomakaWork <vLomakaWork@Vitaliis-MacBook-Pro.local>
2025-01-05 00:25:52 +0200

    fix: issue 11 android

--- android/src/main/java/com/mymathlib/MyMathLibModule.kt
+++ android/src/main/java/com/mymathlib/MyMathLibModule.kt
@@ -1,41 +1,37 @@
 package com.mymathlib
 
-import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
 
-class MyMathLibModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
-
-    @ReactMethod
-    fun add(a: Double, b: Double, callback: Callback) {
-        val result = a + b
-        callback.invoke(result)
-    }
-
-    @ReactMethod
-    fun subtract(a: Double, b: Double, callback: Callback) {
-        val result = a - b
-        callback.invoke(result)
-    }
-
-    @ReactMethod
-    fun multiply(a: Double, b: Double, callback: Callback) {
-        val result = a * b
-        callback.invoke(result)
-    }
-
-    @ReactMethod
-    fun divide(a: Double, b: Double, callback: Callback) {
-        if (b == 0.0) {
-            callback.invoke("Error", "Division by zero is not allowed")
-        } else {
-            val result = a / b
-            callback.invoke(result)
-        }
-    }
+class MathModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
     override fun getName(): String {
-        return "MyMathLib"
+        return "MathModule"
+    }
+
+    @ReactMethod
+    fun add(a: Double, b: Double, promise: Promise) {
+        promise.resolve(a + b)
+    }
+
+    @ReactMethod
+    fun subtract(a: Double, b: Double, promise: Promise) {
+        promise.resolve(a - b)
+    }
+
+    @ReactMethod
+    fun multiply(a: Double, b: Double, promise: Promise) {
+        promise.resolve(a * b)
+    }
+
+    @ReactMethod
+    fun divide(a: Double, b: Double, promise: Promise) {
+        if (b == 0.0) {
+            promise.reject("DIVIDE_BY_ZERO", "Cannot divide by zero")
+        } else {
+            promise.resolve(a / b)
+        }
     }
 }
--- android/src/main/java/com/mymathlib/MyMathLibPackage.kt
+++ android/src/main/java/com/mymathlib/MyMathLibPackage.kt
@@ -1,39 +1,16 @@
 package com.mymathlib
 
-import com.facebook.react.BaseReactPackage
+import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.module.model.ReactModuleInfo
-import com.facebook.react.module.model.ReactModuleInfoProvider
-import java.util.HashMap
+import com.facebook.react.uimanager.ViewManager
 
-class MyMathLibPackage : BaseReactPackage() {
-
-  companion object {
-    const val MODULE_NAME = "MyMathLib" 
-  }
-
-  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
-    return if (name == MODULE_NAME) {
-      MyMathLibModule(reactContext)
-    } else {
-      null
+class MathPackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(MathModule(reactContext))
     }
-  }
 
-  override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
-    return ReactModuleInfoProvider {
-      val moduleInfos: MutableMap<String, ReactModuleInfo> = HashMap()
-      moduleInfos[MODULE_NAME] = ReactModuleInfo(
-        MODULE_NAME,
-        MODULE_NAME,
-        false,  // canOverrideExistingModule
-        false,  // needsEagerInit
-        true,   // hasConstants
-        false,  // isCxxModule
-        true    // isTurboModule
-      )
-      moduleInfos
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
     }
-  }
 }
